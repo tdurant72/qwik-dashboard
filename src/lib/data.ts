a782825cf2b3dca63628ae9209264fa8e74d9b99
Tyus Durant <contact@tyusdurant.com>
2024-07-01 09:57:24 -0400

    end of chapter 9

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -25,7 +25,11 @@ export const fetchRevenue = server$(async function () {
   // Open a new connection
   const pool = await getPool();
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const { rows } = await pool.query<Revenue>('SELECT * FROM revenue');
+
+    console.log('Data fetch completed after 3 seconds.');
     // Close the connection
     await pool.end();
     return rows;
